bf48a867298405157dd2e6834213dcf24147b9c4
warwick <warwick@niigata.(none)>
2012-11-01 17:00:39 +0900

    Create repo

---
+++ tests/module/Album/src/Album/Controller/AlbumControllerTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Album\Controller;
+
+use Album\Controller\AlbumController;
+use Zend\Http\Request;
+use Zend\Http\Response;
+use Zend\Mvc\MvcEvent;
+use Zend\Mvc\Router\RouteMatch;
+use PHPUnit_Framework_TestCase;
+
+class AlbumControllerTest extends PHPUnit_Framework_TestCase
+{
+    protected $controller;
+    protected $request;
+    protected $response;
+    protected $routeMatch;
+    protected $event;
+
+    public function testAddActionCanBeAccessed()
+    {
+        $this->routeMatch->setParam('action', 'add');
+
+        $result   = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertInstanceOf('Zend\View\Model\ViewModel', $result);
+    }
+
+    public function testDeleteActionCanBeAccessed()
+    {
+        $this->routeMatch->setParam('action', 'delete');
+
+        $result   = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertInstanceOf('Zend\View\Model\ViewModel', $result);
+    }
+
+    public function testEditActionCanBeAccessed()
+    {
+        $this->routeMatch->setParam('action', 'edit');
+
+        $result   = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertInstanceOf('Zend\View\Model\ViewModel', $result);
+    }
+
+    public function testIndexActionCanBeAccessed()
+    {
+        $this->routeMatch->setParam('action', 'index');
+
+        $result   = $this->controller->dispatch($this->request);
+        $response = $this->controller->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertInstanceOf('Zend\View\Model\ViewModel', $result);
+    }
+
+    protected function setUp()
+    {
+        $bootstrap        = \Zend\Mvc\Application::init(include 'config/application.config.php');
+        $this->controller = new AlbumController();
+        $this->request    = new Request();
+        $this->routeMatch = new RouteMatch(array('controller' => 'index'));
+        $this->event      = $bootstrap->getMvcEvent();
+        $this->event->setRouteMatch($this->routeMatch);
+        $this->controller->setEvent($this->event);
+        $this->controller->setEventManager($bootstrap->getEventManager());
+        $this->controller->setServiceLocator($bootstrap->getServiceManager());
+    }
+}
